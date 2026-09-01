5cf99f84455a712f2a22a5e8d1f653595e2a88a3
Alessandro <alessandro@confeitaria.local>
2026-09-01 00:26:02 -0300

    Corrige Invalid URL na função serverless da Vercel
    A Vercel entrega o Request com URL relativa (só o path), mas o H3
    do TanStack Start exige URL absoluta. Reconstrói a URL usando os
    headers host e x-forwarded-proto antes de passar pro handler.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import handler from "../dist/server/server.js";
 
 export default function (request) {
-  return handler.fetch(request, {}, {});
+  const proto = request.headers.get("x-forwarded-proto") ?? "https";
+  const host = request.headers.get("host");
+  const url = new URL(request.url, `${proto}://${host}`);
+  const absoluteRequest = new Request(url, request);
+  return handler.fetch(absoluteRequest, {}, {});
 }
